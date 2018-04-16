11e0326bab089204baa951754e96ae186ab08a9b
Gabryel Boeira <gabryel392@gmail.com>
2018-04-16 20:57:16 -0300

    Incluindo Tela de Cadastro de profissional

--- AgendamentoConsulta/ClassLibrary1/Pessoa.cs
+++ AgendamentoConsulta/ClassLibrary1/Pessoa.cs
@@ -1,13 +1,16 @@
-﻿namespace AgendamentoConsulta
+﻿using System;
+
+namespace AgendamentoConsulta
 {
     //cria a classe que corresponde as caracteristicas que seram usados como base 
     public abstract class Pessoa
     {
         public int PessoaID { get; set; }
         public string Nome { get; set; }
-        public string Numero { get; set; }
+        public string Celular { get; set; }
         public string Email { get; set; }
         public string Rg { get; set; }
+        public DateTime DtNascimento { get; set; } 
 
         public int EnderecoID { get; set; } 
     }
