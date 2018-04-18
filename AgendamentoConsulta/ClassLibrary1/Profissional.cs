e24d8ff2d832fbc90d9f5fdb467bf1fb70f8c67e
Marcelo Cezário <marcelocezario@gmail.com>
2018-04-18 16:25:54 -0300

    Criando atributos em Profissional com os dias que atender

--- AgendamentoConsulta/ClassLibrary1/Profissional.cs
+++ AgendamentoConsulta/ClassLibrary1/Profissional.cs
@@ -6,7 +6,14 @@ namespace Model
     class Profissional : Pessoa 
     {
         public string ResgistroProfissional { get; set; }
-        public int DiasSemana { get; set; }
+        //Dias da semana
+        public bool Domingo { get; set; }
+        public bool Segunda { get; set; }
+        public bool Terca { get; set; }
+        public bool Quarta { get; set; }
+        public bool Quinta { get; set; }
+        public bool Sexta { get; set; }
+        public bool Sabado { get; set; }
         public DateTime Periodo { get; set; }
     }
 }
