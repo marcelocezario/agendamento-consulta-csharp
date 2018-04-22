cd5c71f4d29298b4e601bed4d6b483d0adc10dc2
gabryel boeira <gabryel392@gmail.com>
2018-04-22 19:34:10 -0300

    Verificações nos cadastros concluidos

--- AgendamentoConsulta/AgendamentoConsulta/CadastroLocal.xaml.cs
+++ AgendamentoConsulta/AgendamentoConsulta/CadastroLocal.xaml.cs
@@ -16,6 +16,53 @@ namespace AgendamentoConsulta
         {
             InitializeComponent();
         }
+        private bool VerificarPreenchimentoCampos()
+        {
+            if (TimePickerHInicioLocal.Text.Equals(""))
+                MessageBox.Show("Horario de inicio para o uso obrigadorio");
+            else
+            {
+                if (TimePickerHFimLocal.Text.Equals(""))
+                    MessageBox.Show("Horario de Fim do para o uso obrigadorio");
+                else
+                {
+                    if (BoxNomeLocal.Text.Equals(""))
+                        MessageBox.Show("O campo nome é obrigatório");
+                    else
+                    {
+                        if (BoxCepLocal.Text.Equals("__.___-___"))
+                            MessageBox.Show("O campo CEP é obrigatório");
+                        else
+                        {
+                            if (BoxRuaLocal.Text.Equals(""))
+                                MessageBox.Show("O campo rua é obrigatório");
+                            else
+                            {
+                                if (BoxNumeroLocal.Text.Equals(""))
+                                    MessageBox.Show("O campo número em endereço é obrigatório");
+                                else
+                                {
+                                    if (BoxCidadeLocal.Text.Equals(""))
+                                        MessageBox.Show("O campo Cidade é obrigatório");
+                                    else
+                                    {
+                                        if (ComboBoxEstado.Text.Equals(""))
+                                            MessageBox.Show("O campo UF é obrigatório");
+                                        else
+                                        {
+                                            return true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
 
         //salvar nova sala 
         private void ButtonSalvarLocal_Click(object sender, RoutedEventArgs e)
--- AgendamentoConsulta/AgendamentoConsulta/CadastroProfissional.xaml.cs
+++ AgendamentoConsulta/AgendamentoConsulta/CadastroProfissional.xaml.cs
@@ -16,8 +16,76 @@ namespace AgendamentoConsulta
         {
             InitializeComponent();
         }
+        //Verificacao
+        private bool VerificarPreenchimentoCampos()
+        {
+            if (BoxResgistroProfissional.Text.Equals(""))
+                MessageBox.Show("Registro profissional em branco");
+            else
+            {
+                if (TimePickerHInicioProfissional.Text.Equals(""))
+                    MessageBox.Show("Horario de inicio do atendimento obrigadorio");
+                else
+                {
+                    if (TimePickerHFimProfissional.Text.Equals(""))
+                        MessageBox.Show("Horario de Fim do atendimento obrigadorio");
+                    else
+                    {
+                        if (BoxNomeProfissional.Text.Equals(""))
+                            MessageBox.Show("O campo nome é obrigatório");
+                        else
+                        {
+                            if (Verificacao.Verificacao.ValidaCpf(BoxCpfProfissional.Text))
+                                MessageBox.Show("O campo CPF é obrigatório");
+                            else
+                            {
+                                if (DatePickerDtNascimentoProfissional.Text.Equals(""))
+                                    MessageBox.Show("O campo data de nascimento é obrigatório");
+                                else
+                                {
+                                    if (!Verificacao.Verificacao.ValidarEmail(BoxEmailProfissional.Text))
+                                        MessageBox.Show("E-mail inválido");
+                                    else
+                                    {
+                                        if (BoxCepProfissional.Text.Equals("__.___-___"))
+                                            MessageBox.Show("O campo CEP é obrigatório");
+                                        else
+                                        {
+                                            if (BoxRuaProfissional.Text.Equals(""))
+                                                MessageBox.Show("O campo rua é obrigatório");
+                                            else
+                                            {
+                                                if (BoxNumeroProfissional.Text.Equals(""))
+                                                    MessageBox.Show("O campo número em endereço é obrigatório");
+                                                else
+                                                {
+                                                    if (BoxCidadeProfissional.Text.Equals(""))
+                                                        MessageBox.Show("O campo Cidade é obrigatório");
+                                                    else
+                                                    {
+                                                        if (ComboBoxEstado.Text.Equals(""))
+                                                            MessageBox.Show("O campo UF é obrigatório");
+                                                        else
+                                                        {
+                                                            return true;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
 
-        //valida para o textbox aceitar somente numeros
+        /// <summary>
+        /// Validações não podem ir para a lib de validações pois são excutadas em execução
+        /// </summary>
         private void BoxRgProfissional_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {         
             try
@@ -33,7 +101,11 @@ namespace AgendamentoConsulta
                 MessageBox.Show("Erro: " + ex);
             }
         }
-        
+        private void ButtonSalvarProfissional_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         //valida para o textbox aceitar somente numeros
         private void BoxNumeroProfissional_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
@@ -50,5 +122,7 @@ namespace AgendamentoConsulta
                 MessageBox.Show("Erro: " + ex);
             }
         }
+
+      
     }
 }
