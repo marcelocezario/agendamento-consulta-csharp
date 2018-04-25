d93187ffbf28e540adf36a264b0b869fcd297fd2
Marcelo Cezário <marcelocezario@gmail.com>
2018-04-25 14:40:13 -0300

    Tela de agendamento em andamento

--- AgendamentoConsulta/AgendamentoConsulta/Agendamento.xaml.cs
+++ AgendamentoConsulta/AgendamentoConsulta/Agendamento.xaml.cs
@@ -31,7 +31,7 @@ namespace AgendamentoConsulta
 
         private void ComboBoxNomeProfissional_Loaded(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void ButtonSalvarAgendamento_Click(object sender, RoutedEventArgs e)
@@ -39,17 +39,65 @@ namespace AgendamentoConsulta
 
         }
 
-        private void ButtonBuscarProfissional_Click(object sender, RoutedEventArgs e)
+        private Profissional ButtonBuscarProfissional_Click(object sender, RoutedEventArgs e)
         {
             ProfissionalController pc = new ProfissionalController();
             Profissional profissional;
 
-            profissional = pc.PesquisarPorNome(BoxBuscaNomeProfissional.Text);
+            profissional = pc.PesquisarPorNome(BoxBuscarProfissional.Text);
 
-            if (MessageBox.Show(profissional.Nome, "Confirma o profissional?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-                MessageBox.Show(profissional.Nome + " adicionado com sucesso!");
+            if (profissional != null)
+            {
+                string profissionalBusca = "Id: " + profissional.ID +
+                    "\nNome: " + profissional.Nome +
+                    "\nEspecialidade: " + profissional.Especialidade +
+                    "\nRegistro Profissional: " + profissional.ResgistroProfissional;
+
+                if (MessageBox.Show(profissionalBusca, "Confirma o profissional?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    BoxBuscarProfissional.Text = profissional.Nome;
+                    MessageBox.Show(profissional.Nome + " adicionado com sucesso!");
+
+                    return profissional;
+                }
+                else
+                    MessageBox.Show("Nenhum profissional selecionado");
+            }
             else
-                MessageBox.Show("Nenhum profissional selecionado");
+                MessageBox.Show("Nenhum profissional encontrado");
+
+            return null;
+
+        }
+
+        private Paciente ButtonBuscarPaciente_Click(object sender, RoutedEventArgs e)
+        {
+            PacienteController pc = new PacienteController();
+            Paciente paciente;
+
+            paciente = pc.PesquisarPorNome(BoxBuscasrPaciente.Text);
+
+            if (paciente != null)
+            {
+                string pacienteBusca = "Id: " + paciente.ID +
+                    "\nNome: " + paciente.Nome +
+                    "\nCPF: " + paciente.Cpf +
+                    "\nData de nascimento: " + paciente.DtNascimento.ToString("dd/MM/yyyy");
+
+                if (MessageBox.Show(pacienteBusca, "Confirma o paciente?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    BoxBuscasrPaciente.Text = paciente.Nome;
+                    MessageBox.Show(paciente.Nome + " adicionado com sucesso!");
+
+                    return paciente;
+                }
+                else
+                    MessageBox.Show("Nenhum paciente selecionado");
+            }
+            else
+                MessageBox.Show("Nenhum paciente encontrado");
+
+            return null;
         }
     }
 }
